48cc5e9f7f40e0a788f650ba458fba6c52e59875
AbhijeetBatania <abhijeet.batania2000@gmail.com>
2026-07-15 12:50:55 +0530

    Time: 0 ms (100%), Space: 8.5 MB (46.67%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
+    int gcd(int a, int b) {  //Euclidean Algorithm
+        while (b != 0) {
+            int rem = a % b;
+            a = b;
+            b = rem;
+        }
+        return a;
+    }
     int gcdOfOddEvenSums(int n) {
-        return n;
+        int oddSum = n * n;
+        int evenSum = n * (n + 1);
+
+        return gcd(evenSum, oddSum);
     }
 };
