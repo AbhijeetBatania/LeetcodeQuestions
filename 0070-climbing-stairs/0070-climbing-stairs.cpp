103ddc5b631d81736d1a4c066edcf8d169b753ce
AbhijeetBatania <abhijeet.batania2000@gmail.com>
2026-08-17 13:21:19 +0530

    Time: 0 ms (100%), Space: 8 MB (59.28%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
-    int t[46];
-
-    int solve(int n){
-        if(n<0) return 0;
-        if(n==0) return 1; 
-
-        if(t[n] != -1) 
-            return t[n];
-
-        int one_step =  solve(n-1);
-        int two_step = solve(n-2);  
-
-        return t[n] = one_step + two_step;
-    }
     int climbStairs(int n) {
-        memset(t, -1, sizeof(t));
+        if(n==1 || n==2) return n;
 
-        return solve(n);
+        int a = 1, b = 2;
+        int c;
+
+        for(int i = 3 ; i <= n ; i++){
+            c = a+b;
+            a = b;
+            b = c;
+        }
+        return c;
     }
 };
